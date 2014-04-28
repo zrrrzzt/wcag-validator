9364297f111d26cc0878676ed0b2174c4e81d1da
Geir Gåsodden <geir.gasodden@pythonia.no>
2014-04-28 20:52:01 +0200

    Adds test

--- test/testInput.js
+++ test/testInput.js
@@ -5,12 +5,28 @@ var wcag = require('../index')
 describe('wcag - inputs', function(){
 
   it('Should throw if uri is not supplied', function(done){
-    var opts = {'format':'json'};
+    var opts = {'id':'12345'};
     wcag(opts, function(err, data){
       assert.throws(function(){
           if(err) throw err;
         }, function(err){
           if((err instanceof Error) && /Missing required param: uri/.test(err)){
+            return true
+          }
+        },
+        "Unexpected error"
+      );
+      done();
+    });
+  });
+
+  it('Should throw if uri is not supplied', function(done){
+    var opts = {'uri':'https://github.com/zrrrzzt'};
+    wcag(opts, function(err, data){
+      assert.throws(function(){
+          if(err) throw err;
+        }, function(err){
+          if((err instanceof Error) && /Missing required param: id/.test(err)){
             return true
           }
         },
