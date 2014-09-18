ec9fb6f3325d6dc09b49f3ab2c3d858a9489a34b
Geir Gåsodden <geir.gasodden@pythonia.no>
2014-09-18 08:55:17 +0200

    Adds test for valid uri

--- test/testInput.js
+++ test/testInput.js
@@ -20,6 +20,25 @@ describe('wcag - inputs', function(){
     });
   });
 
+  it('Should throw if param uri contains invalid url', function(done){
+
+    var opts = {id:'12345', uri:'pysje'};
+
+    wcag(opts, function(err, data){
+      assert.throws(function(){
+          if(err) throw err;
+        }, function(err){
+          if((err instanceof Error) && /Invalid url/.test(err)){
+            return true
+          }
+        },
+        "Unexpected error"
+      );
+      done();
+    });
+
+  });
+
   it('Should throw if param id is not supplied', function(done){
     var opts = {'uri':'https://github.com/zrrrzzt'};
     wcag(opts, function(err, data){
