39f2c40525f9504a14869652d71a712b7b119c4c
Geir Gåsodden <geir.gasodden@pythonia.no>
2015-05-20 21:08:42 +0200

    Changes to match jscs

--- cli.js
+++ cli.js
@@ -1,15 +1,14 @@
 #!/usr/bin/env node
 'use strict';
 
-var fs = require('fs')
-  , validator = require('./index')
-  , pkg = require('./package.json')
-  , query = process.argv[2]
-  , argv = require('minimist')((process.argv.slice(2)))
-  , opts = {}
-  ;
+var validator = require('./index');
+var pkg = require('./package.json');
+var query = process.argv[2];
+var argv = require('minimist')((process.argv.slice(2)));
+var opts = {};
 
 function printHelp() {
+  var msg = '$ wcag-validator <uri> --id=<id> --output=<output> --guide=<guide> --offset=<offset>';
   console.log(pkg.description);
   console.log('');
   console.log('Usage:');
@@ -17,7 +16,7 @@ function printHelp() {
   console.log('');
   console.log('Optional params');
   console.log('See http://achecker.ca/documentation/web_service_api.php for more');
-  console.log('  $ wcag-validator <uri> --id=<id> --output=<output> --guide=<guide> --offset=<offset>');
+  console.log(msg);
 }
 
 if (!query || process.argv.indexOf('-h') !== -1 || process.argv.indexOf('--help') !== -1) {
@@ -30,31 +29,33 @@ if (process.argv.indexOf('-v') !== -1 || process.argv.indexOf('--version') !== -
   return;
 }
 
-if(query.indexOf('http') !== -1) {
+if (query.indexOf('http') !== -1) {
   opts.uri = argv._[0];
 }
 
-if(argv.uri){
+if (argv.uri) {
   opts.uri = argv.uri;
 }
 
-if(argv.id){
+if (argv.id) {
   opts.id = argv.id;
 }
 
-if(argv.output){
+if (argv.output) {
   opts.output = argv.output;
 }
 
-if(argv.guide){
+if (argv.guide) {
   opts.guide = argv.guide;
 }
 
-if(argv.offset){
+if (argv.offset) {
   opts.offset = argv.offset;
 }
 
-validator(opts, function(err, data){
-  if(err) throw err;
+validator(opts, function(err, data) {
+  if (err) {
+    throw err;
+  }
   console.log(data);
 });
